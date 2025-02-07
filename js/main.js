ddfb8cd86723ce5092c18a3e4e73aa12180109c7
sbigma <ziel@gmail.com>
2025-02-07 09:50:51 +0700

    arrow function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
-let angka = prompt("masukan angka");
+// let angka = prompt("masukan angka");
 
-if (angka %2 == 0) {
-    alert(angka + " angko ganok ");
-} else {
-    alert(angka + " angko ganjia ");
-}
+// if (angka %2 == 0) {
+//     alert(angka + " angko ganok ");
+// } else {
+//     alert(angka + " angko ganjia ");
+// }
+
+
+// hari jumat belajar arrow function
+function greeting_card() {
+    console.log("Hai Zeeyfx")
+  }
+  
+  greeting_card()
+  
+  function perkalian(a, b) {
+    console.log(a*b)
+  }
+  
+  perkalian (3, 5)
+  
+  function pembagian(a, b) {
+    console.log(a/b)
+  }
+  
+  pembagian (9, 3)
+  
+  function penambahan(a, b) {
+    console.log(a+b)
+  }
+  
+  penambahan (9, 3)
+  
+  function pengurangan(a, b) {
+    console.log(a-b)
+  }
+  
+  pengurangan (9, 3)
+  
+  function is_even(num) {
+    if (num % 2 == 0){
+      return("Ganok") 
+    } else {
+      return("Ganjia")
+    }
+  }
+  
+  let coba = is_even(13)
+  console.log (coba)
+  
+  // arrow function
+  const add_number =  (a,b) => { console.log(a+b) }
+  
+  add_number(1, 8)
+  
