238d9797cd1f7776818dee720dfd4a7025bf8a56
sbigma <ziel@gmail.com>
2025-02-08 15:27:16 +0700

    web login done

--- DOM/main.js
+++ DOM/main.js
@@ -1,4 +1,4 @@
-// document.title = "Camp Traders";
+document.title = "Camp Traders";
 
 // const body = document.body;
 // body.style.backgroundColor = "cyan";
@@ -10,25 +10,60 @@
 // body.append(h1);
 
 let h1 = document.getElementById("ZeeyFx");
-h1.style.display = "none";
-
 let h1_2 = document.getElementById("try");
-h1_2.style.display = "none";
 
 let username = document.getElementById("Username");
 let password = document.querySelector("#Password");
 
+let input = document.querySelector(".input");
+
+let login = document.getElementById("login");
+let logout = document.getElementById("logout");
+
+h1.style.display = "none";
+h1_2.style.display = "none";
+input.style.display = "block";
+login.style.display = "block";
+logout.style.display = "none";
 
 function onLogin() {
-    const username = document.getElementById("Username").value;
-    const password = document.getElementById("Password").value;
-    if (username === "admin" && password === "1234") {
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("password", password.value);
+
+    if (username.value == "admin" && password.value == 1234) {
         alert("Login Success");
         h1.style.display = "block";
-        console.log(username);
-        console.log(password);
+        h1_2.style.display = "none";
+        input.style.display = "none";
+        login.style.display = "none";
+        logout.style.display = "block";
+
+        localStorage.setItem("role", "admin");
+        localStorage.setItem("password", "1234");
+
     } else {
         alert("Login Failed");
+        h1.style.display = "none";
         h1_2.style.display = "block";
+        input.style.display = "none";
+        login.style.display = "none";
+        logout.style.display = "block";
+
+        localStorage.setItem("role", "user");
+        localStorage.setItem("password", "1234");
     }
+} 
+
+if (localStorage.getItem("username")) {
+    h1.style.display = "block";
+    h1_2.style.display = "none";
+    input.style.display = "none";
+    login.style.display = "none";
+    logout.style.display = "block";
+}   
+
+function onLogout() {
+    localStorage.clear();
+    alert("Logout Success");
+    location.reload();
 }
